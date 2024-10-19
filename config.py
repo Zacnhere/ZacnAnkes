b03f465b445d8e125e02801a1a33e1e56e857776
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-10-20 03:05:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", "24623085"))
     API_HASH = os.getenv("API_HASH", "75ce0c6125ae201c9e3d5a825c667a91")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7139966122:AAE6zafE_nDxGLttejwYW6YEGlqPCGaOPoU")
-    OWNER_ID = int(os.getenv("OWNER_ID", "1825618929"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7068529485:AAHevXamyL-PtSfvu5l-91D6BY9i4dwlwzo")
+    OWNER_ID = int(os.getenv("OWNER_ID", "1361379181"))
     MONGO_URL = os.getenv(
         "MONGO_URL",
-        "mongodb+srv://trlynice:oc9VGD2dgYrKiQ5H@cluster0.tolyzhy.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://ZacnAnkes:ZacnAnkes@cluster0.vcbtd.mongodb.net/?retryWrites=true&w=majority&appName=ZACNHERE",
     )
 
