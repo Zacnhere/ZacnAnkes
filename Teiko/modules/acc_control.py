41cb3181b9cfd04cd54c0a828138695068fc72dc
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-11-19 02:42:13 +0700

    Update acc_control.py

--- Teiko/modules/acc_control.py
+++ Teiko/modules/acc_control.py
@@ -1,143 +1,119 @@
-
-from pyrogram import *
-from pyrogram.enums import *
-from pyrogram.types import *
-from pyrogram.errors import *
-
+from pyrogram import filters
+from pyrogram.enums import ChatType
 from datetime import datetime, timedelta
-from pytz import timezone
-
-from Teiko import *
-
+import asyncio
 
 @PY.BOT("profile")
-async def _(client, message):
-    if len(message.command) > 1:
-        input_identifier = message.command[1]
-    else:
-        return await message.reply("<b>Usage chat_id or @ group!</b>")
+async def profile_handler(client, message):
+    if len(message.command) < 2:
+        return await message.reply("<b>Usage: /profile [chat_id or @group]</b>")
 
+    input_identifier = message.command[1]
     chat_id = await extract_id(message, input_identifier)
+
     if not chat_id:
         return await message.reply("<b>Group not found!</b>")
+
     try:
         chat = await client.get_chat(chat_id)
-        if chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
-        else:
-            response = f"{chat_id}"
-        exp = await DB.get_exp(chat_id)
-        if exp:
-            time = exp
-        else:
-            time = "Null"
-        status = await DB.get_vars(TB.me.id, f"chat_{chat_id}")
-        await message.reply(f"""
+        response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
+        exp = await DB.get_exp(chat_id) or "Null"
+        status = await DB.get_vars(TB.me.id, f"chat_{chat_id}") or "Inactive"
+
+        return await message.reply(f"""
 <b>Profiles Ankes!</b>
 
- <b>Group:</b> {response}
- <b>Status:</b> {status}
- <b>Expired:</b> {time}
+<b>Group:</b> {response}
+<b>Status:</b> {status}
+<b>Expired:</b> {exp}
 """)
-    except Exception as error:
-        await message.reply(error)
+    except Exception as e:
+        return await message.reply(f"<b>Error:</b> {str(e)}")
 
 
 @PY.BOT("addankes")
-async def _(client, message):
+async def add_ankes_handler(client, message):
+    if len(message.command) < 2:
+        return await message.reply("<b>Usage: /addankes [chat_id or @group] [days]</b>")
+
     try:
-        if len(message.command) > 1:
-            input_identifier = message.command[1]
-            get_day = int(message.command[2]) if len(message.command) > 2 else 30
-        else:
-            return await message.reply("<b>Usage: /command chat_id or @group [days]</b>")
+        input_identifier = message.command[1]
+        get_day = int(message.command[2]) if len(message.command) > 2 else 30
 
         if message.from_user.id not in (Config.OWNER_ID, *await DB.get_list_vars(TB.me.id, "seller")):
-            return await message.reply("<b>You do not have access to use this command here.</b>")
+            return await message.reply("<b>You do not have access to use this command.</b>")
 
         chat_id = await extract_id(message, input_identifier)
-        ankes = await DB.get_list_vars(TB.me.id, "ankes_group")
-        chat = await client.get_chat(chat_id)
-
-        if not chat.id:
+        if not chat_id:
             return await message.reply("<b>Invalid chat ID or username provided!</b>")
-            
-        if chat.id in ankes:
-            return await message.reply("<b>Group chat is already in the ankes list!</b>")
+
+        ankes = await DB.get_list_vars(TB.me.id, "ankes_group")
+        if chat_id in ankes:
+            return await message.reply("<b>Group is already in the Ankes list!</b>")
 
         expiry_date = (datetime.now() + timedelta(days=get_day)).strftime("%d.%m.%Y")
-        await DB.add_exp(chat.id, expiry_date)
-        await DB.add_list_vars(TB.me.id, "ankes_group", chat.id)
+        await DB.add_exp(chat_id, expiry_date)
+        await DB.add_list_vars(TB.me.id, "ankes_group", chat_id)
 
-        if chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
-        else:
-            response = f"{chat.id}"
+        chat = await client.get_chat(chat_id)
+        response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
 
-        message_content = f"""
+        return await message.reply(f"""
 <b>Information!</b>
 
 <b>Group:</b> {response}
-<b>Expired:</b> {get_day} day(s)
-<b>Reason:</b> Added to ankes list
-"""
-        return await message.reply(message_content)
-
+<b>Expired:</b> {expiry_date}
+<b>Reason:</b> Added to Ankes list
+""")
     except ValueError:
-        return await message.reply("<b>Error: Invalid number of days provided. Please provide a valid integer.</b>")
+        return await message.reply("<b>Error: Invalid number of days provided.</b>")
     except Exception as e:
         return await message.reply(f"<b>An unexpected error occurred:</b> {str(e)}")
 
 
 @PY.BOT("remankes")
-async def _(client, message):
+async def remove_ankes_handler(client, message):
+    if len(message.command) < 2:
+        return await message.reply("<b>Usage: /remankes [chat_id or @group]</b>")
+
     try:
-        if len(message.command) > 1:
-            input_identifier = message.command[1]
-        else:
-            return await message.reply("<b>Usage: /command chat_id or @group</b>")
+        input_identifier = message.command[1]
 
         if message.from_user.id not in (Config.OWNER_ID, *await DB.get_list_vars(TB.me.id, "seller")):
-            return await message.reply("<b>You do not have access to use this command here.</b>")
+            return await message.reply("<b>You do not have access to use this command.</b>")
 
         chat_id = await extract_id(message, input_identifier)
-        ankes = await DB.get_list_vars(TB.me.id, "ankes_group")
-        chat = await client.get_chat(chat_id)
-        
-        if not chat.id:
+        if not chat_id:
             return await message.reply("<b>Invalid chat ID or username provided!</b>")
-        
-        if chat.id not in ankes:
-            return await message.reply("<b>Group chat is not in the ankes list!</b>")
-        
-        await DB.rem_exp(chat.id)
-        await DB.remove_list_vars(TB.me.id, "ankes_group", chat.id)
-        
-        if chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
-        else:
-            response = f"{chat.id}"
-        
-        message_content = f"""
+
+        ankes = await DB.get_list_vars(TB.me.id, "ankes_group")
+        if chat_id not in ankes:
+            return await message.reply("<b>Group is not in the Ankes list!</b>")
+
+        await DB.rem_exp(chat_id)
+        await DB.remove_list_vars(TB.me.id, "ankes_group", chat_id)
+
+        chat = await client.get_chat(chat_id)
+        response = f"<a href=https://t.me/{chat.username or chat.id}>{chat.title}</a>"
+
+        return await message.reply(f"""
 <b>Information!</b>
 
 <b>Group:</b> {response}
 <b>Expired:</b> Null
-<b>Reason:</b> Removed from ankes list
-"""
-        return await message.reply(message_content)
-
+<b>Reason:</b> Removed from Ankes list
+""")
     except Exception as e:
         return await message.reply(f"<b>An unexpected error occurred:</b> {str(e)}")
 
 
 @PY.BOT("listankes|lankes")
-async def _(client, message):
+async def list_ankes_handler(client, message):
     mzg = await message.reply("<b>Processing...</b>")
 
     try:
         if message.from_user.id not in (Config.OWNER_ID, *await DB.get_list_vars(TB.me.id, "seller")):
-            return await mzg.edit("<b>You do not have access to use this command here.</b>")
+            return await mzg.edit("<b>You do not have access to use this command.</b>")
 
         ankes = await DB.get_list_vars(TB.me.id, "ankes_group")
         if not ankes:
@@ -147,20 +123,12 @@ async def _(client, message):
         for chat_id in ankes:
             try:
                 chat = await client.get_chat(int(chat_id))
-                if chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-                    chat_list.append(
-                        f'<a href="https://t.me/{chat.username or chat.id}">{chat.title}</a>'
-                    )
-            except Exception as e:
+                response = f'<a href="https://t.me/{chat.username or chat.id}">{chat.title}</a>'
+                chat_list.append(response)
+            except Exception:
                 chat_list.append(f"<code>{chat_id}</code>")
                 continue
 
-        if chat_list:
-            response = "<b>Ankes Group List:</b>\n\n" + "\n".join(chat_list)
-            return await mzg.edit(response)
-        else:
-            return await mzg.edit("<b>No valid groups found in the Ankes list!</b>")
-    
+        return await mzg.edit("<b>Ankes Group List:</b>\n\n" + "\n".join(chat_list))
     except Exception as e:
         return await mzg.edit(f"<b>An unexpected error occurred:</b> {str(e)}")
-
