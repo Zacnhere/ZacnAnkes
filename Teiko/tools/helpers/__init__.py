4067f256e79507ba3c9ae46553a3dd067aba90c4
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-11-19 03:30:13 +0700

    Delete Teiko/tools directory

--- Teiko/tools/helpers/__init__.py
+++
@@ -1,5 +0,0 @@
-from Teiko.tools.helpers.deleter import *
-from Teiko.tools.helpers.misc import *
-from Teiko.tools.helpers.handlers import *
-from Teiko.tools.helpers.inline import *
-from Teiko.tools.helpers.text import *
