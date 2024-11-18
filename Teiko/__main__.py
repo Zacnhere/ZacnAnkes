44f21440a0a06559f5f4e47c4fcc4f0963246c07
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-11-19 02:56:55 +0700

    Update __main__.py

--- Teiko/__main__.py
+++ Teiko/__main__.py
@@ -1,16 +1,22 @@
 import os
 import asyncio
-
 from Teiko import *
 
-
+# Fungsi utama untuk memulai bot dan menjalankan plugin
 async def main():
+    # Memulai bot
     await TB.start()
+    
+    # Menjalankan plugin dan fungsi tambahan
     await asyncio.gather(loadPlugins(), exp_ankes())
+    
+    # Menghapus file session yang ada setelah bot dimulai
     os.system("rm -rf *session*")
+    
+    # Menunggu agar bot tetap berjalan
     await asyncio.Event().wait()
 
-
+# Jika file ini dijalankan langsung, mulai event loop
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop_policy().get_event_loop()
-    loop.run_until_complete(main())
+    # Menggunakan asyncio.run untuk memulai aplikasi
+    asyncio.run(main())
